da3fea6b571488cd20e7dea4f36056ecd77c8b04
Jarod <74381052+Tarkorr@users.noreply.github.com>
2022-11-07 21:13:03 +0100

    Update
    merging old functions,
    adding some "s",
    grid added,
    shadow added.

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # Snake #
-## The classic game of snake implemented in [Pyxel](https://github.com/kitao/pyxel)! ##
+
+## The classic game of snake implemented in [Pyxel](https://github.com/kitao/pyxel) ##
 
 Try and collect the tasty apples without running
 into the side or yourself.
 
-Requires pyxel version >= 1.1.6.
+Requires pyxel version >= 1.8.22.
 
-Controls are the arrow keys **←** **↑** **→** **↓**
+Controls are the arrow keys: **←** **↑** **→** **↓**
 
 **Q**: Quit the game
 **R**: Restart the game
 
-![Screenshot!](https://github.com/timbledum/snake/blob/master/snake.png)
+<center> <img src="snake.png" alt="drawing" width="400"/> </center>
 
 ## Features ##
 
@@ -27,3 +28,5 @@ Controls are the arrow keys **←** **↑** **→** **↓**
 2. Install [Pyxel](https://github.com/kitao/pyxel) using their instructions
 3. Clone or copy this repository
 4. `python3 snake.py` at the command line
+
+> This is a fork using the latest version of pyxel in date (and adpated the changes because recent versions of pyxel doesn't support this old version), adding some fancy features like shadow for scores and texts. The fancy grid too ! <br> Used for a school project :)
--- snake.py
+++ snake.py
@@ -11,6 +11,7 @@ Q: Quit the game
 R: Restart the game
 
 Created by Marcus Croucher in 2018.
+Updated By Tarkorr in 2022.
 """
 
 from collections import namedtuple, deque
@@ -23,22 +24,12 @@ Point = namedtuple("Point", ["x", "y"])  # Convenience class for coordinates
 # Constants #
 #############
 
-COL_BACKGROUND = 3
-COL_BODY = 11
-COL_HEAD = 7
-COL_DEATH = 8
-COL_APPLE = 8
-
-TEXT_DEATH = ["GAME OVER", "(Q)UIT", "(R)ESTART"]
-COL_TEXT_DEATH = 0
-HEIGHT_DEATH = 5
-
 WIDTH = 40
 HEIGHT = 50
 
+TEXT_DEATH = ["GAME OVER", "(Q)UIT", "(R)ESTART"]
 HEIGHT_SCORE = pyxel.FONT_HEIGHT
-COL_SCORE = 6
-COL_SCORE_BACKGROUND = 5
+HEIGHT_DEATH = 5
 
 UP = Point(0, -1)
 DOWN = Point(0, 1)
@@ -58,8 +49,9 @@ class Snake:
     def __init__(self):
         """Initiate pyxel, set up initial game variables, and run."""
 
-        pyxel.init(WIDTH, HEIGHT, caption="Snake!", scale=8, fps=20)
+        pyxel.init(WIDTH, HEIGHT, fps=10, quit_key=pyxel.KEY_Q, title="Snake !")
         self.music = Music()
+        self.col = Colors()
         self.reset()
         pyxel.run(self.update, self.draw)
 
@@ -92,22 +84,18 @@ class Snake:
 
         if pyxel.btnp(pyxel.KEY_R):
             self.reset()
+            
+        if pyxel.btnp(pyxel.KEY_T):
+            self.snake.append(self.popped_point)
+            self.score += 1
 
     def update_direction(self):
         """Watch the keys and change direction."""
 
-        if pyxel.btn(pyxel.KEY_UP):
-            if self.direction is not DOWN:
-                self.direction = UP
-        elif pyxel.btn(pyxel.KEY_DOWN):
-            if self.direction is not UP:
-                self.direction = DOWN
-        elif pyxel.btn(pyxel.KEY_LEFT):
-            if self.direction is not RIGHT:
-                self.direction = LEFT
-        elif pyxel.btn(pyxel.KEY_RIGHT):
-            if self.direction is not LEFT:
-                self.direction = RIGHT
+        if   pyxel.btn(pyxel.KEY_UP)    and self.direction is not DOWN:  self.direction = UP
+        elif pyxel.btn(pyxel.KEY_DOWN)  and self.direction is not UP:    self.direction = DOWN
+        elif pyxel.btn(pyxel.KEY_LEFT)  and self.direction is not RIGHT: self.direction = LEFT
+        elif pyxel.btn(pyxel.KEY_RIGHT) and self.direction is not LEFT:  self.direction = RIGHT 
 
     def update_snake(self):
         """Move the snake based on the direction."""
@@ -159,46 +147,54 @@ class Snake:
         """Draw the background, snake, score, and apple OR the end screen."""
 
         if not self.death:
-            pyxel.cls(col=COL_BACKGROUND)
+            self.draw_background()
             self.draw_snake()
             self.draw_score()
-            pyxel.pix(self.apple.x, self.apple.y, col=COL_APPLE)
+            pyxel.rect(self.apple.x, self.apple.y, 1, 1, col=self.col.apple_color)
 
         else:
             self.draw_death()
-
+    
+    def draw_background(self):
+        for x in range(WIDTH + 1):
+            for y in range(HEIGHT + 1):
+                pyxel.rect(x, y, 1, 1, self.col.background1_color if (x-y)%2==0 else self.col.background2_color)
+    
     def draw_snake(self):
         """Draw the snake with a distinct head by iterating through deque."""
 
         for i, point in enumerate(self.snake):
             if i == 0:
-                colour = COL_HEAD
+                colour = self.col.head_color
             else:
-                colour = COL_BODY
-            pyxel.pix(point.x, point.y, col=colour)
+                colour = self.col.body_color
+            pyxel.rect(point.x, point.y, 1, 1, col=colour)
 
     def draw_score(self):
         """Draw the score at the top."""
 
         score = "{:04}".format(self.score)
-        pyxel.rect(0, 0, WIDTH, HEIGHT_SCORE, COL_SCORE_BACKGROUND)
-        pyxel.text(1, 1, score, COL_SCORE)
+        pyxel.rect(0, 0, WIDTH, HEIGHT_SCORE + 1, self.col.background_score_color)
+        pyxel.text(1, 1, score, self.col.score_color_fade)
+        pyxel.text(0, 1, score, self.col.score_color)
+        
 
     def draw_death(self):
         """Draw a blank screen with some text."""
 
-        pyxel.cls(col=COL_DEATH)
+        pyxel.cls(col=self.col.death_color)
         display_text = TEXT_DEATH[:]
         display_text.insert(1, "{:04}".format(self.score))
         for i, text in enumerate(display_text):
             y_offset = (pyxel.FONT_HEIGHT + 2) * i
             text_x = self.center_text(text, WIDTH)
-            pyxel.text(text_x, HEIGHT_DEATH + y_offset, text, COL_TEXT_DEATH)
+            pyxel.text(text_x+1, HEIGHT_DEATH + y_offset, text,self.col.death_text_color_fade)
+            pyxel.text(text_x, HEIGHT_DEATH + y_offset, text, self.col.death_text_color)
 
     @staticmethod
     def center_text(text, page_width, char_width=pyxel.FONT_WIDTH):
         """Helper function for calcuating the start x value for centered text."""
-
+        
         text_width = len(text) * char_width
         return (page_width - text_width) // 2
 
@@ -207,6 +203,34 @@ class Snake:
 # Music and sound effects #
 ###########################
 
+class Colors:
+    def __init__(self):
+        pyxel.colors[0] = 0x0F3A00  # head_snake
+        pyxel.colors[1] = 0x1D5200  # body_snake
+        pyxel.colors[2] = 0xB81800  # apple
+        pyxel.colors[3] = 0x81dc4a  # background_1
+        pyxel.colors[4] = 0x63c02e  # background_2
+        pyxel.colors[5] = 0x5DF1FF  # score_txt
+        pyxel.colors[6] = 0x00b2ff  # score_bg
+        pyxel.colors[7] = 0x554244  # GameOver_txt
+        pyxel.colors[8] = 0x3c2c2e  # GameOver_bg
+        pyxel.colors[9] = 0x008cb1  # score_txt_fade
+        pyxel.colors[10] = 0x271811 # GameOver_txt_fade
+        
+        self.head_color = 0
+        self.body_color = 1
+        
+        self.apple_color = 2
+        self.background1_color = 3
+        self.background2_color = 4
+        
+        self.score_color = 5
+        self.score_color_fade = 9
+        self.background_score_color = 6
+        
+        self.death_text_color = 7
+        self.death_text_color_fade = 10
+        self.death_color = 8
 
 class Music:
     def __init__(self):
@@ -214,14 +238,18 @@ class Music:
 
         # Sound effects
         pyxel.sound(0).set(
-            note="c3e3g3c4c4", tone="s", volume="4", effect=("n" * 4 + "f"), speed=7
+            notes="c3e3g3c4c4",
+            tones="s",
+            volumes="4",
+            effects=("n" * 4 + "f"),
+            speed=7
         )
         pyxel.sound(1).set(
-            note="f3 b2 f2 b1  f1 f1 f1 f1",
-            tone="p",
-            volume=("4" * 4 + "4321"),
-            effect=("n" * 7 + "f"),
-            speed=9,
+            notes="f3 b2 f2 b1  f1 f1 f1 f1",
+            tones="p",
+            volumes=("4" * 4 + "4321"),
+            effects=("n" * 7 + "f"),
+            speed=9
         )
 
         melody1 = (
@@ -248,10 +276,10 @@ class Music:
 
         # Music
         pyxel.sound(2).set(
-            note=melody1 * 2 + melody2 * 2,
-            tone="s",
-            volume=("3"),
-            effect=("nnnsffff"),
+            notes=melody1 * 2 + melody2 * 2,
+            tones="s",
+            volumes=("3"),
+            effects=("nnnsffff"),
             speed=20,
         )
 
@@ -267,13 +295,17 @@ class Music:
         )
 
         pyxel.sound(3).set(
-            note=harmony1 * 2 + harmony2 * 2, tone="t", volume="5", effect="f", speed=20
+            notes=harmony1 * 2 + harmony2 * 2,
+            tones="t", 
+            volumes="5", 
+            effects="f", 
+            speed=20
         )
         pyxel.sound(4).set(
-            note=("f0 r a4 r  f0 f0 a4 r" "f0 r a4 r   f0 f0 a4 f0"),
-            tone="n",
-            volume="6622 6622 6622 6426",
-            effect="f",
+            notes=("f0 r a4 r  f0 f0 a4 r" "f0 r a4 r   f0 f0 a4 f0"),
+            tones="n",
+            volumes="6622 6622 6622 6426",
+            effects="f",
             speed=20,
         )
 
